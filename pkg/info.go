cb5b85ddbf9de83f4789d938f90a00cacd66da20
spectrex02 <iscale821@gmail.com>
2019-10-12 02:24:36 +0900

    first commit.

---
+++ pkg/info.go
@@ -0,0 +1,144 @@
+package goref
+
+import (
+	"fmt"
+)
+type Info interface {
+	Show()
+}
+
+type FunctionInfo struct {
+	Info
+	Id       int
+	Name     string
+	Receiver interface{}	//string or nil
+	ReceiverType interface{}	//receiver type (string or nil)
+	ReturnType interface{}	//return type (string or nil)
+	Called   []int
+	Package  string
+	Body Block	//function body
+}
+
+//struct for storing information of struct
+type StructInfo struct {
+	Info
+	Id int
+	Name string
+	//Member map[string]string
+	Member []string
+	MethodList []FunctionInfo
+	Package string
+}
+
+//struct for storing information of interface
+type InterfaceInfo struct {
+	Info
+	Id int
+	Name string
+	//Methods map[string][]string
+	Methods []string
+	Package string
+}
+
+type VarInfo struct {
+	Info
+	Id int
+	Name string
+	Type string
+}
+
+
+//struct for get a method or function list called by some function
+type CalledFunction struct {
+	Info
+	Receiver string
+	Name string
+}
+
+
+//struct for block stmt {...}
+type Block struct {
+	Info
+	Called []CalledFunction
+	AssignVars []VarInfo
+	Sub []SubBlock
+}
+
+type SubBlock struct {
+	Block
+}
+
+
+func (c *CalledFunction) Show() {
+	fmt.Printf("recv -> %s : name -> %s\n", c.Receiver, c.Name)
+}
+
+//interface some type
+type Id interface {
+	AllocateId() int
+}
+//id type for function
+type FuncId struct {
+	Id int
+}
+
+//id type for function
+type StructId struct {
+	Id int
+}
+type InterfaceId struct {
+	Id int
+}
+type VarId struct {
+	Id int
+}
+
+func (fId *FuncId) AllocateId() int {
+	fId.Id = fId.Id + 1
+	return fId.Id
+}
+
+func (sId *StructId) AllocateId() int {
+	sId.Id = sId.Id + 1
+	return sId.Id
+}
+
+func (iId *InterfaceId) AllocateId() int {
+	iId.Id = iId.Id + 1
+	return iId.Id
+}
+
+func (vId *VarId) AllocateId() int {
+	vId.Id = vId.Id + 1
+	return vId.Id
+}
+
+func (f FunctionInfo) Show() {
+	fmt.Println("-----show function info-----")
+	fmt.Println("Id:", f.Id)
+	fmt.Println("Name:", f.Name)
+	fmt.Println("Package:", f.Package)
+	fmt.Println("Receiver:", f.Receiver)
+	fmt.Println("Receiver Type:", f.ReceiverType)
+	fmt.Println("Called:", f.Called)
+	fmt.Println("Body:", f.Body)
+	fmt.Printf("\n")
+}
+
+func (s StructInfo) Show() {
+	fmt.Println("-----show struct info-----")
+	fmt.Println("Id:", s.Id)
+	fmt.Println("Name:", s.Name)
+	fmt.Println("Package:", s.Package)
+	fmt.Println("Member:", s.Member)
+	fmt.Println("Methods:", s.MethodList)
+	fmt.Printf("\n")
+}
+
+func (i InterfaceInfo) Show() {
+	fmt.Println("-----show interface info-----")
+	fmt.Println("Id:", i.Id)
+	fmt.Println("Name:", i.Name)
+	fmt.Println("Methods:", i.Methods)
+}
+
---
+++ pkg/path.go
@@ -0,0 +1,173 @@
+package goref
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	//"log"
+	"path/filepath"
+)
+
+type Node interface {
+	Show()
+	IsDir() bool
+}
+
+type Dir struct {
+	Node
+	Child []Node
+	Name string
+	Path string
+	Package string
+
+}
+
+type File struct {
+	Node
+	Name string
+	Path string
+	Info parse.FileInfo
+	Package string
+}
+
+//don't use Root struct
+type Root struct {
+	Node
+	Nodes []Node
+	RootPath string
+	Package string
+}
+func (f File) IsDir() bool { return false}
+func (d Dir) IsDir() bool { return true}
+func (root Root) IsDir() bool { return true}
+
+func (f File) Show() {
+	fmt.Printf("File name -> %v\n", f.Name)
+	//f.Info.Show()
+}
+
+func (d Dir) Show() {
+	fmt.Printf("Directory name and package name -> %v, %v\n", d.Name, d.Package)
+	for _, child := range d.Child {
+		if child.IsDir() {
+			child.Show()
+			continue
+		}
+		child.Show()
+	}
+}
+
+func (root Root) Show() {
+	//fmt.Printf("Root directory name -> %v\n", root.RootPath)
+	for _, node := range root.Nodes {
+		if node.IsDir() {
+			node.Show()
+			continue
+		}
+		node.Show()
+	}
+}
+
+func New(path string) Dir {
+	p := parse.New()
+	nodes, packageName := getTree(path, p)
+	root := Dir{
+		Child: nodes,
+		Name: trim(path),
+		Path: path,
+		Package: packageName,
+	}
+	root.GivePackageName()
+	return root
+}
+
+//trim root directory name
+func trim(path string) string {
+	if strings.Contains(path, "/") {
+		index := strings.LastIndex(path, "/")
+		length := len(path)
+		name := path[index+1 : length]
+		return name
+	}
+	return path
+}
+
+
+func getTree(path string, p *parse.Parser) ([]Node, string) {
+	//fmt.Println("now in getTree function. path ->", path)
+	var nodes []Node
+	var name string
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		//if file is directory, we have to search files in this directory recursively
+		if file.IsDir() {
+			child, packageName := getTree(filepath.Join(path, file.Name()), p)
+			dir := Dir{
+				Child: child,
+				Name:file.Name(),
+				Path:path,
+				Package: packageName,
+			}
+			nodes = append(nodes, dir)
+			continue
+		}
+		//if file is file
+		//we have to check whether go file or not.
+		if !strings.Contains(file.Name(), ".go") { continue }
+		//fmt.Println("file path ===== ", filepath.Join(path, file.Name()))
+		info := p.GetFileInfo(filepath.Join(path, file.Name()))
+		f := File{
+			Name: file.Name(),
+			Path: filepath.Join(path, file.Name()),
+			Info: info,
+			Package: info.Package,
+		}
+		//fmt.Println(f.Name)
+		nodes = append(nodes, f)
+		name = info.Package
+	}
+	//fmt.Println("length of nodes: ", len(nodes))
+	return nodes, name
+}
+
+//initially, Dir struct can't have its own package name.
+//so this method give package name field to it.
+func (d *Dir) GivePackageName() {
+	for _, node := range d.Child {
+		if node.IsDir() {
+			node.(Dir).givePackageName()
+		}
+	}
+}
+
+func (d Dir) givePackageName() {
+	for _, node := range d.Child {
+		if node.IsDir() {
+			node.(Dir).givePackageName()
+			continue
+		}
+
+		d.Package = node.(File).Package
+	}
+}
+
+//traverse package and return Dir.
+//Because some files belong to one package, so to identify some package, Dir is better.
+func (d Dir) TraversePackage(packageName string) (Dir, bool) {
+	for _, node := range d.Child {
+		if node.IsDir() {
+			if node.(Dir).Package == packageName {
+				return node.(Dir), true
+			}
+			_, _ = node.(Dir).TraversePackage(packageName)
+		}
+	}
+	if packageName == d.Package {
+		return d, true
+	}
+	return d, false
+}
